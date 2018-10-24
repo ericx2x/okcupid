4a582d163662850929e5897737272103a1deae3d
ericx2x <ericjlima@gmail.com>
2018-10-24 10:52:25 -0400

    Update okcupid0.sh

--- okcupid0.sh
+++ okcupid0.sh
@@ -1,4 +1,5 @@
 #xdotool type Um. Hi. Anyways, I feel you appear attractive and consequently would like to explore the possibility of enhancing your life by means of exposure to my awesomeness.;
+#My dear creature, you seem ight. I would like to explore the possibility of enhancing your life with my blissful sheltering ability.
 #sleep 1;
 xdotool keydown alt key Tab; 
 sleep .5; 
